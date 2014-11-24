5f7bdda68909d9c571028057ddb76c6ce2f8e746
Nicola Hauke <jasinai@jasinai.de>
2014-11-24 23:43:19 +0100

    Move the json files to a json folder

--- start.rb
+++ start.rb
@@ -2,11 +2,11 @@ require './lts'
 `mkdir -p gen`
 `rm -rf gen/*`
 switch_lts = LTS.new
-switch_lts.parse_json("switch.json")
+switch_lts.parse_json("json/switch.json")
 switch_lts.to_tex("switch")
 `pdflatex --output-directory gen/ gen/switch.tex`
 light_lts = LTS.new
-light_lts.parse_json("light.json")
+light_lts.parse_json("json/light.json")
 light_lts.to_tex("light")
 `pdflatex --output-directory gen/ gen/light.tex`
 lightCompSwitch = light_lts.compose(switch_lts, ["press", "hold"])
@@ -14,11 +14,11 @@ lightCompSwitch.to_tex("comp")
 `pdflatex --output-directory gen/ gen/comp.tex`
 
 tl1 = LTS.new
-tl1.parse_json("TL1.json")
+tl1.parse_json("json/TL1.json")
 tl1.to_tex("TL1")
 `pdflatex --output-directory gen/ gen/TL1.tex`
 tl2 = LTS.new
-tl2.parse_json("TL2.json")
+tl2.parse_json("json/TL2.json")
 tl2.to_tex("TL2")
 TL1u2 = tl1.compose(tl2, [])
 TL1u2.to_tex("TL1u2")
